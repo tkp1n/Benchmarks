584d3a26a0941f519a7b7eafaa4d3af5b9f2091a
Sebastien Ros <sebastienros@gmail.com>
2019-05-20 11:04:31 -0700

    Using Fluid templates

--- src/Benchmarks/Middleware/MiddlewareHelpers.cs
+++ src/Benchmarks/Middleware/MiddlewareHelpers.cs
@@ -3,16 +3,35 @@
 
 using System.Collections.Generic;
 using System.Globalization;
+using System.IO;
 using System.Text;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 using Benchmarks.Data;
+using Fluid;
 using Microsoft.AspNetCore.Http;
 
 namespace Benchmarks.Middleware
 {
     public static class MiddlewareHelpers
     {
+        private const string FortunesTemplate = @"
+<!DOCTYPE html>
+<html>
+<head><title>Fortunes</title></head>
+<body>
+    <table>
+        <tr><th>id</th><th>message</th></tr>
+        {% for item in fortunes %}
+            <tr><td>{{ item.Id }}</td><td>{{ item.Message }}</td></tr>
+        {% endfor %}
+    </table>
+</body>
+</html>
+";
+
+        private static FluidTemplate Template = FluidTemplate.Parse(FortunesTemplate);
+
         public static int GetMultipleQueriesQueryCount(HttpContext httpContext)
         {
             var queries = 1;
@@ -30,7 +49,7 @@ namespace Benchmarks.Middleware
                     : 1;
         }
 
-        public static Task RenderFortunesHtml(IEnumerable<Fortune> model, HttpContext httpContext, HtmlEncoder htmlEncoder)
+        public static Task RenderFortunesHtml2(IEnumerable<Fortune> model, HttpContext httpContext, HtmlEncoder htmlEncoder)
         {
             httpContext.Response.StatusCode = StatusCodes.Status200OK;
             httpContext.Response.ContentType = "text/html; charset=UTF-8";
@@ -51,6 +70,21 @@ namespace Benchmarks.Middleware
             // fortunes includes multibyte characters so response.Length is incorrect
             httpContext.Response.ContentLength = Encoding.UTF8.GetByteCount(response);
             return httpContext.Response.WriteAsync(response);
+        }
+
+        public static async Task RenderFortunesHtml(IEnumerable<Fortune> model, HttpContext httpContext, HtmlEncoder htmlEncoder)
+        {
+            httpContext.Response.StatusCode = StatusCodes.Status200OK;
+            httpContext.Response.ContentType = "text/html; charset=UTF-8";
+
+            var templateContext = new TemplateContext();
+            templateContext.SetValue("fortunes", model);
+
+            var response = await Template.RenderAsync(templateContext, htmlEncoder);
+
+            // fortunes includes multibyte characters so response.Length is incorrect
+            httpContext.Response.ContentLength = Encoding.UTF8.GetByteCount(response);
+            await httpContext.Response.WriteAsync(response);
         }
     }
 }
