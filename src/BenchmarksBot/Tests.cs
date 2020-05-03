fe8c172bd241c9ec06a96259b72b3009a578d804
Sébastien Ros <sebastienros@gmail.com>
2020-05-03 10:55:29 -0700

    Removing test

--- src/BenchmarksBot/Tests.cs
+++ src/BenchmarksBot/Tests.cs
@@ -1,21 +1,21 @@
-﻿using Xunit;
+﻿//using Xunit;
 
-namespace BenchmarksBot
-{
-    public class Tests
-    {
-        [Fact]
-        public void ShouldMatchExitingScenario()
-        {
-            var tags = Tags.Match("GRPCUnary");
-            Assert.NotEmpty(tags);
-        }
+//namespace BenchmarksBot
+//{
+//    public class Tests
+//    {
+//        [Fact]
+//        public void ShouldMatchExitingScenario()
+//        {
+//            var tags = Tags.Match("GRPCUnary");
+//            Assert.NotEmpty(tags);
+//        }
 
-        [Fact]
-        public void ShouldNotMatchScenario()
-        {
-            var tags = Tags.Match("NoGRPC");
-            Assert.Empty(tags);
-        }
-    }
-}
+//        [Fact]
+//        public void ShouldNotMatchScenario()
+//        {
+//            var tags = Tags.Match("NoGRPC");
+//            Assert.Empty(tags);
+//        }
+//    }
+//}
